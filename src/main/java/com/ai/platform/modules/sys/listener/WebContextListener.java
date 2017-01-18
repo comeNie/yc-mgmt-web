4b7aceca62fe064cea83599f89988ec615fd9770
mb3333mb <mengbo@asiainfo.com>
2017-01-18 15:07:20 +0800

    项目启动时，清空redis缓存

--- src/main/java/com/ai/platform/modules/sys/listener/WebContextListener.java
+++ src/main/java/com/ai/platform/modules/sys/listener/WebContextListener.java
@@ -4,15 +4,22 @@ import javax.servlet.ServletContext;
 
 import org.springframework.web.context.WebApplicationContext;
 
+import com.ai.opt.sdk.components.mcs.MCSClientFactory;
+import com.ai.paas.ipaas.mcs.interfaces.ICacheClient;
 import com.ai.platform.modules.sys.service.SystemService;
 
 public class WebContextListener extends org.springframework.web.context.ContextLoaderListener {
-	   
+	private final static ICacheClient _jedisGnarea = MCSClientFactory.getCacheClient("com.ai.platform.common.cache.gnarea");
+
 	@Override
 	public WebApplicationContext initWebApplicationContext(ServletContext servletContext) {
+		_jedisGnarea.del("areaTreeALL","OfficeAllList","findTreeInit");
 		if (!SystemService.printKeyLoadMessage()){
 			return null;
 		}
+		
+		
+		
 		return super.initWebApplicationContext(servletContext);
 	}
 }
